b37f3107b8e3fcb1c0fb6585f6236b46af8950fd
liao2000 <liao2000@livemail.tw>
2021-06-22 01:24:18 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,9 +4,10 @@ apt install python3.7
 apt install build-essential cmake
 apt install libopenblas-dev liblapack-dev
 apt install libx11-dev libgtk-3-dev
-apt install python-pip
+apt install python3-pip
 python -m pip install --upgrade pip
 pip3 install dlib
 pip3 install face-recognition
 pip3 install pillow
+pip3 install scikit-build
 pip3 install opencv-python
